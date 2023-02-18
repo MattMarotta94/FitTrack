34525cf8d0b25a6bf484b85b64f2205f0079bcc0
Matt Marotta <mcmarotta48@gmail.com>
2023-02-18 13:53:29 -0500

    making changes to front end

--- web/src/pages/createWorkout.js
+++ web/src/pages/createWorkout.js
@@ -65,11 +65,15 @@ class CreateWorkout extends BindingClass {
 
     async searchCardio(evt) {
         evt.preventDefault();
+        const cardioButton = document.getElementById('cardio-button');
+        const origButtonText = cardioButton.innerText;
+        cardioButton.innerText = "Loading..";
 
         const Cardio = "Cardio";
-
         const jsonCardioList = await this.client.getExercises(`${Cardio}`);
         console.log(jsonCardioList);
+
+        cardioButton.innerText = origButtonText;
         
         for (var i = 0; i < jsonCardioList.length; i++) {
             var exercise = jsonCardioList[i];
@@ -80,11 +84,15 @@ class CreateWorkout extends BindingClass {
 
     async searchCalisthenics(evt) {
         evt.preventDefault();
+        const calisthenicsButton = document.getElementById('calisthenics-button');
+        const origButtonText = calisthenicsButton.innerText;
+        calisthenicsButton.innerText = "Loading..";
 
         const Calisthenics = "Calisthenics";
-
         const jsonCalisthenicsList = await this.client.getExercises(`${Calisthenics}`);
         console.log(jsonCalisthenicsList);
+
+        calisthenicsButton.innerText = origButtonText;
         
         for (var i = 0; i < jsonCalisthenicsList.length; i++) {
             var exercise = jsonCalisthenicsList[i];
@@ -96,11 +104,15 @@ class CreateWorkout extends BindingClass {
 
     async searchWeightLifting(evt) {
         evt.preventDefault();
+        const weightliftingButton = document.getElementById('weightlifting-button');
+        const origButtonText = weightliftingButton.innerText;
+        weightliftingButton.innerText = "Loading..";
 
         const WeightLifting = "WeightLifting";
-
         const jsonWeightLiftingList = await this.client.getExercises(`${WeightLifting}`);
         console.log(jsonWeightLiftingList);
+
+        weightliftingButton.innerText = origButtonText;
         
         for (var i = 0; i < jsonWeightLiftingList.length; i++) {
             var exercise = jsonWeightLiftingList[i];
